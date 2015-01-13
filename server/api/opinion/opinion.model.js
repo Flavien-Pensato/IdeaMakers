ab6aa97ba7785bd768863b26c6acf92640fffa4b
antonhansel <antoninribeaud@gmail.com>
2015-01-13 14:12:40 +0100

    Adding the opinion model

--- server/api/opinion/opinion.model.js
+++ server/api/opinion/opinion.model.js
@@ -4,9 +4,9 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 var OpinionSchema = new Schema({
-  name: String,
-  info: String,
-  active: Boolean
+  owner: String,
+  idea: String,
+  type: {type: String, enum: ['LIKE', 'DISLIkE']}
 });
 
 module.exports = mongoose.model('Opinion', OpinionSchema);
